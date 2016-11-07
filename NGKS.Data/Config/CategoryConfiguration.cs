d6d454694d2aba2cd262e61d86dfe8600f74dfc4
Harshana Narangoda <harshanamcrox10@gmail.com>
2016-11-07 18:02:36 +0530

    added comment configuration class,and category configuration class

---
+++ NGKS.Data/Config/CategoryConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using NGKS.Data.Config;
+using NGKS.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NGKS.Data.Config
+{
+    public class CategoryConfiguration : EntityBaseConfiguration<Category>
+    {
+        public CategoryConfiguration()
+        {
+            Property(c => c.Name).IsRequired().HasMaxLength(50);
+        }
+    }
+}
---
+++ NGKS.Data/Config/CommentConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using NGKS.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NGKS.Data.Config
+{
+    public class CommentConfiguration : EntityBaseConfiguration<Comment>
+    {
+        public CommentConfiguration()
+        {
+            Property(cm => cm.PostID).IsRequired();
+            Property(cm => cm.ParentCommentID).IsRequired();
+            Property(cm => cm.Message).IsRequired().HasMaxLength(500);
+            Property(cm => cm.AddedDate).IsRequired();
+            Property(cm => cm.UserID).IsRequired();
+            Property(cm => cm.UpdateTime);
+
+        }
+    }
+}
