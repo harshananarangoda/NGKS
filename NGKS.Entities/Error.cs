5b3a6a4058432959f3de28e2efa726ff34c6ef9c
Harshana Narangoda <harshanamcrox10@gmail.com>
2016-11-07 11:37:06 +0530

    Add error class to the entity project

---
+++ NGKS.Entities/Error.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NGKS.Entities
+{
+    public class Error : IEntityBase
+    {
+        public int ID { get; set; }
+        public string Message { get; set; }
+        public string StackTrace { get; set; }
+        public DateTime DateTimeOccured { get; set; }
+    }
+}
