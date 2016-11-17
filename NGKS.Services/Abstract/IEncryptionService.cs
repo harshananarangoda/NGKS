56014c170815935ee209de7bcc06f9a43f0a8286
Harshana Narangoda <harshanamcrox10@gmail.com>
2016-11-17 14:12:34 +0530

    Add Encryption Service Interface

---
+++ NGKS.Services/Abstract/IEncryptionService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NGKS.Services.Abstract
+{
+    /// <summary>
+    /// Interface: IEncryptionService
+    /// </summary>
+    public interface IEncryptionService
+    {
+        /// <summary>
+        /// Create Salt
+        /// </summary>
+        /// <returns>string (Salt)</returns>
+        string CreateSalt();
+
+        /// <summary>
+        /// Get Encrypted Password
+        /// </summary>
+        /// <param name="password">password</param>
+        /// <param name="salt">salt</param>
+        /// <returns>string(Encrypted Password)</returns>
+        string EncryptedPassword(string password, string salt);
+    }
+}
