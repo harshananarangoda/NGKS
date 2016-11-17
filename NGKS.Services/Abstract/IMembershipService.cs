e39ddf1ae147e130433e0efa28ce17dcdf1f3ecf
Harshana Narangoda <harshanamcrox10@gmail.com>
2016-11-17 15:52:03 +0530

    Added Membership service interface

---
+++ NGKS.Services/Abstract/IMembershipService.cs
@@ -0,0 +1,47 @@
+﻿using NGKS.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NGKS.Services.Abstract
+{
+    /// <summary>
+    /// Interface: IMembershipService
+    /// </summary>
+    public interface IMembershipService
+    {
+        /// <summary>
+        /// Validate User
+        /// </summary>
+        /// <param name="username">username</param>
+        /// <param name="password">password</param>
+        /// <returns>MembershipCntext</returns>
+        MembershipCntext ValidateUser(string username, string password);
+
+        /// <summary>
+        /// Create a user
+        /// </summary>
+        /// <param name="username">username</param>
+        /// <param name="email">email</param>
+        /// <param name="password">password</param>
+        /// <param name="roles">roles</param>
+        /// <returns>User</returns>
+        User CreateUSer(string username, string email, string password, int[] roles);
+
+        /// <summary>
+        /// Get User
+        /// </summary>
+        /// <param name="userID">user ID</param>
+        /// <returns>User</returns>
+        User GetUser(int userID);
+
+        /// <summary>
+        /// Get List of Roles
+        /// </summary>
+        /// <param name="username">username</param>
+        /// <returns>List<Roles></returns>
+        List<Role> GetUserRoles(string username);
+    }
+}
