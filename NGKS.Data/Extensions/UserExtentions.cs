7448fe0b8474281ff70a779f720e21b264f1f872
Harshana Narangoda <harshanamcrox10@gmail.com>
2016-11-19 00:15:14 +0530

    Add User Extions Class, added user by username method.

---
+++ NGKS.Data/Extensions/UserExtentions.cs
@@ -0,0 +1,27 @@
+﻿using NGKS.Data.Repositories;
+using NGKS.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NGKS.Data.Extensions
+{
+    /// <summary>
+    /// Class: User Extentions
+    /// </summary>
+    public static class UserExtentions
+    {
+        /// <summary>
+        /// Extention method to get user by username
+        /// </summary>
+        /// <param name="userRepository">User repository</param>
+        /// <param name="username">username</param>
+        /// <returns>User</returns>
+        public static User GetSingleByUsername(this IEntityRepository<User> userRepository, string username)
+        {
+            return userRepository.GetAll().FirstOrDefault(x => x.Username == username);
+        }
+    }
+}
