8759e720ec554b94e120e810dd50eefdc72ddbfc
Harshana Narangoda <harshanamcrox10@gmail.com>
2016-11-23 16:12:40 +0530

    Added bundle config, Boostrapper class

---
+++ NGKS.Web/App_Start/Bootstrapper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NGKS.Web.App_Start
+{
+    /// <summary>
+    /// Class: Bootstrapper
+    /// </summary>
+    public class Bootstrapper
+    {
+    }
+}
