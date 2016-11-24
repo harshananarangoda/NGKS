1ff46e8b1017d37c05be4851b1efa4748614aa2c
Harshana Narangoda <harshanamcrox10@gmail.com>
2016-11-24 16:00:48 +0530

    Added authentication handler class

---
+++ NGKS.Web/Infrastructure/MessageHandler/NGKSAuthHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+
+namespace NGKS.Web.Infrastructure.MessageHandler
+{
+    /// <summary>
+    /// Class: NGKSAuthHandler
+    /// </summary>
+    public class NGKSAuthHandler : DelegatingHandler
+    {
+
+    }
+}
