4fb21f594e904cafcc30fc3418a6ccfb8d19d5fa
Harshana Narangoda <harshanamcrox10@gmail.com>
2016-11-28 10:27:10 +0530

    Configuration changes

--- NGKS.Web/App_Start/WebApiConfig.cs
+++ NGKS.Web/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NGKS.Web.Infrastructure.MessageHandler;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
@@ -10,6 +11,7 @@ namespace NGKS.Web
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            config.MessageHandlers.Add(new NGKSAuthHandler());
 
             // Web API routes
             config.MapHttpAttributeRoutes();
--- NGKS.Web/Infrastructure/MessageHandler/NGKSAuthHandler.cs
+++ NGKS.Web/Infrastructure/MessageHandler/NGKSAuthHandler.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NGKS.Web.Infrastructure.Extensions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
